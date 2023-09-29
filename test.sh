a305232f62cdaa6f9792d39f76295edae47df6e9
alexpricker <alex.pricker@gmail.com>
2023-09-29 18:32:46 +0300

    123

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+RESPONSE=$(/bin/python3 /etc/zabbix/scripts/external_system/get_data.charges.py);
+echo "emp.prod.host charges.response $RESPONSE" | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i - &>/dev/null
+exit 0
