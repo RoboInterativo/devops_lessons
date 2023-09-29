6a4338b9434346ccd687745d98929ff66e653a83
alexpricker <alex.pricker@gmail.com>
2023-09-29 17:06:57 +0300

    123

--- curl/test4.sh
+++ curl/test4.sh
@@ -8,7 +8,7 @@ spawn telnet localhost 5000
 expect "'^]'."
 send "POST /test_json HTTP/1.1\r"
 send "Host: localhost\r"
-send "Content-Type: application/json\r"
+send "Content-Type: application/json; charset=utf-8\r"
 send "Accept-Ranges: bytes\r"
 send "Content-Length: 46\r"
 #send "{\"login\":\"my_login\",\"password\":\"my_password\"}\r\r"
