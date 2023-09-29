e519532243dff3d80d6912a57bbdcd50f45135a8
alexpricker <alex.pricker@gmail.com>
2023-09-29 17:40:19 +0300

    123

--- curl/test4.sh
+++ curl/test4.sh
@@ -10,7 +10,7 @@ send "POST /test_json HTTP/1.1\r"
 send "Host: localhost\r"
 send "Content-Type: application/json\r"
 send "Accept-Ranges: bytes\r"
-send "Content-Length: 46"
+send "Content-Length: 46\r\r"
 send "\r"
 send '{"login":"my_login","password":"my_password"}'
 send "\r\r"
